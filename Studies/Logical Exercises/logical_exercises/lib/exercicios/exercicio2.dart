55836b70584c6051b705c4644c6454cb8cd30e15
Thiago Aksenen <aksenen1.thiago@gmail.com>
2025-09-27 23:29:04 -0300

    Atualização fittedBox para evitar Overflow

--- Studies/Logical Exercises/logical_exercises/lib/exercicios/exercicio2.dart
+++ Studies/Logical Exercises/logical_exercises/lib/exercicios/exercicio2.dart
@@ -23,70 +23,79 @@ class _Exercicio2State extends State<Exercicio2> {
           icon: Icon(Icons.close),
         ),
       ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.blue[700],
-          ),
-          height: 550,
-          width: 350,
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                Text(
-                  "O Valor digitado:",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-                Text(
-                  valor.toString(),
-                  style: TextStyle(color: Colors.white, fontSize: 120),
-                ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.blue[700],
+            ),
+            height: 550,
+            width: 350,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Text(
+                    "O Valor digitado:",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown, 
+                    child: Text(
+                      valor?.toString() ?? "Nenhum",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 120, // tamanho máximo
+                      ),
+                    ),
+                  ),
 
-                Text(
-                  resultado,
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
+                  Text(
+                    resultado,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
 
-                SizedBox(height: 200),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Digite seu número aqui:",
+                  SizedBox(height: 200),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Digite seu número aqui:",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.isEmpty) {
+                          valor = null;
+                          resultado = "não é um número!";
+                        } else {
+                          valor = int.parse(value);
+                          if (valor!.isEven) {
+                            resultado = "é par!";
+                          } else {
+                            resultado = "é impar!";
+                          }
+                        }
+                      });
+                    },
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
                     style: TextStyle(color: Colors.white),
                   ),
-                ),
-                TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      if (value.isEmpty) {
-                        valor = null;
-                        resultado = "não é um número!";
-                      } else {
-                        valor = int.parse(value);
-                        if (valor!.isEven) {
-                          resultado = "é par!";
-                        } else {
-                          resultado = "é impar!";
-                        }
-                      }
-                    });
-                  },
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
