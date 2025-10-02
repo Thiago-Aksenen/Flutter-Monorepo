9139ea5669a673dd0f346752e6b0f7c9cdafed47
Thiago Aksenen <aksenen1.thiago@gmail.com>
2025-10-02 16:45:15 -0300

    Tabuada de número aleatório com List Generate

--- Studies/Logical Exercises/logical_exercises/lib/exercicios/exercicio5.dart
+++ Studies/Logical Exercises/logical_exercises/lib/exercicios/exercicio5.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Exercicio5 extends StatefulWidget {
   const Exercicio5({super.key});
@@ -8,8 +9,83 @@ class Exercicio5 extends StatefulWidget {
 }
 
 class _Exercicio5State extends State<Exercicio5> {
+  int numero = 0;
+  TextEditingController controllerT = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tabuada"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.close),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Digite um valor para ver a tabuada:"),
+            SizedBox(
+              height: 100,
+              width: 70,
+
+              child: TextField(
+                controller: controllerT,
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    if (value.isEmpty) {
+                      controllerT.text = "0";
+                    }
+                    numero = int.parse(controllerT.text);
+                  });
+                },
+
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...List.generate(
+                      5,
+                      (index) => Text(
+                        "${index + 1} * $numero = ${(index + 1) * numero}",
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...List.generate(
+                      5,
+                      (index) => Text(
+                        "${index + 6} * $numero = ${(index + 6) * numero}",
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
